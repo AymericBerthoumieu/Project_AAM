2c0c6dc6509aa7ce2406e17844dac2dfd647cbd3
AymericBerthoumieu <aym3ric2009@hotmail.fr>
2022-04-25 14:05:07 +0200

    add statistics

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
-import utils
+import utils  # if your IDE does not manage to import utils,
+# you will need to import sys and give it the working path using sys.path.append
 
 nb_clusters = 2
 returns = pd.read_excel("./DATA_PROJECT.xlsx", index_col=0)  # monthly returns of stocks
@@ -16,6 +17,8 @@ depth_momentum = 12
 init_level = 100
 test_luck = True
 nb_rd_portfolio = 100  # portfolios to test the null hypothesis of luck
+cvar = 95
+q = 95
 
 # weights of the portfolio
 weights = pd.DataFrame(columns=returns.columns, index=returns.index)
@@ -23,7 +26,7 @@ weights_luck = [pd.DataFrame(columns=returns.columns, index=returns.index) for _
 # momentum scores based on returns for the full sample
 returns_momentum_scores = utils.momentum(depth_momentum, returns, False)
 # volatilities
-volatilities = utils.volatility(12, returns, np.sqrt(12))  # TODO: volatility depth? 12 is the same as momentum
+volatilities = utils.volatility(6, returns, np.sqrt(12))
 
 for i in tqdm(range(max(depth_momentum, depth_regression), len(returns.index))):
     # MOMENTUM SCORE
@@ -75,8 +78,11 @@ for i in tqdm(range(max(depth_momentum, depth_regression), len(returns.index))):
 returns_strat = (weights * returns).dropna().sum(axis=1)
 level_strat = returns_strat.add(1).cumprod()
 level_strat = init_level * level_strat / level_strat[0]
+market_level = market[depth_regression:].add(1).cumprod()
+market_level = init_level * market_level / market_level[0]
 
-stats = utils.Statistics(returns_strat, freq=12, rfr=rfr, CVaR_conf=0.95).run()
+stats_market = utils.Statistics(market, freq=12, rfr=rfr, CVaR_conf=cvar/100).run()
+stats = utils.Statistics(returns_strat, freq=12, rfr=rfr, CVaR_conf=cvar/100).run()
 
 # RETURNS AND LEVELS OF RANDOM PORTFOLIOS
 if test_luck:
@@ -84,10 +90,42 @@ if test_luck:
     level_luck = list(map(lambda x: x.add(1).cumprod(), returns_luck))
     level_luck = list(map(lambda x: init_level * x / x[0], level_luck))
 
+    perf = list()
+    ann_return = list()
+    ann_vol = list()
+    sharpe = list()
+    max_drawdown = list()
+    c_var = list()
     for p in range(nb_rd_portfolio):
         plt.plot(level_luck[p], c="grey", alpha=0.3)
+        stats_luck = utils.Statistics(returns_luck[p], freq=12, rfr=rfr, CVaR_conf=cvar/100).run()
+        perf.append(stats_luck["performance"])
+        ann_return.append(stats_luck["annualized return"])
+        ann_vol.append(stats_luck["annualized volatility"])
+        sharpe.append(stats_luck["Sharpe ratio"])
+        max_drawdown.append(stats_luck["maximum drawdown"]["full period"])
+        c_var.append(stats_luck[f"CVaR ({cvar}%)"])
 
-plt.plot(level_strat)
+    # Statistics
+    p = np.percentile(perf, q)
+    r = np.percentile(ann_return, q)
+    vol = np.percentile(ann_vol, q)
+    sr = np.percentile(sharpe, q)
+    md = np.percentile(max_drawdown, q)
+    cv = np.percentile(c_var, q)
+
+    print(f"Performance strategy: {stats['performance']}, market: {stats_market['performance']}, luck: {p}")
+    print(f"Annualized return strategy: {stats['annualized return']}, market: {stats_market['annualized return']}, luck: {r}")
+    print(f"Annualized volatility strategy: {stats['annualized volatility']}, market: {stats_market['annualized volatility']}, luck: {vol}")
+    print(f"Sharpe strategy: {stats['Sharpe ratio']}, market: {stats_market['Sharpe ratio']}, luck: {sr}")
+    print(f"Maximum drawdown strategy: {stats['maximum drawdown']['full period']}, market: {stats_market['maximum drawdown']['full period']}, luck: {md}")
+    print(f"CVaR strategy: {stats[f'CVaR ({cvar}%)']}, market: {stats_market[f'CVaR ({cvar}%)']}, luck: {cv}")
+
+plt.plot(market_level, label="Eurostoxx 50")
+plt.plot(level_strat, label="Strategy")
 plt.title("Strategy track record")
 plt.xlabel("Dates")
 plt.ylabel("Level")
+plt.legend()
+
+
